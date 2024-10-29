bca7326cd9118e56327261eff6e861065fbb517d
Akt99 <arnabkumartripathy@gmail.com>
2024-10-29 20:17:31 +0530

    email will be sent to the user along with the message in the form filled by the user

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests as req
+import smtplib
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+EMAIL=os.getenv("EMAIL")
+PASSWORD=os.getenv("PASSWORD")
+
 try:
     response = req.get("https://api.npoint.io/cf8f7c02bafac481bab0")
     print(f"Status Code: {response.status_code}")
@@ -27,9 +35,19 @@ def get_all_posts():
 @app.route("/about")
 def about():
     return render_template("about.html")
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post=None
@@ -37,7 +55,12 @@ def show_post(index):
         if blog_post["id"]== index:
             requested_post=blog_post
     return render_template("post.html",post=requested_post)
-
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message} "
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(EMAIL, email , email_message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
